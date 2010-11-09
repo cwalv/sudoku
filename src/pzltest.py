57cae087a7dcb5a8e06cdd98130a331a45eab0d7
charlie <charlie@localhost>
2010-11-09 09:08:36 -0600

    second run; have same easy points done and enough structure to start with the next level

--- src/pzltest.py
+++ src/pzltest.py
@@ -4,49 +4,51 @@ import os.path as p
 import logging
 
 import unittest
-from entry import SudokuPuzzle
+from entry import *
 
 
 
 class Test(unittest.TestCase):
 
-    testd = r'C:\sudoku'
+    testd = r'\\ZD\Users\charlie\Documents\eclipse_workspace\sudoku'
 
-    def testName(self):
-        pass
-
-    def testRead(self):
-        for fn in os.listdir(self.testd):
-            if fn.endswith('.in'):
-                pth = p.join(self.testd, fn)
-                print '%s: \n%s'% (fn, SudokuPuzzle.fromRawFile(pth))
+#    def testRead(self):
+#        for fn in os.listdir(self.testd):
+#            if fn.endswith('.in'):
+#                pth = p.join(self.testd, fn)
+#                print '%s: \n%s'% (fn, SudokuPuzzle().initFromBinFile(pth))
     
-    def testGetRow(self):
-        fn = '11-1.in'
-        pzl = SudokuPuzzle.fromRawFile(p.join(self.testd, fn))
-        expected = [4, 6, 2, 0, 0, 5, 0, 0, 0]
-        found = pzl._getRow(0)
-        self.assertTrue(found == expected)
-        expected = [7, 0, 8, 0, 0, 9, 0, 0, 0]
-        found = pzl._getRow(2)
-        self.assertTrue(found == expected)
+    def setup2cells(self):
+        rgrp, cgrp, sgrp = RowCellGroup(0), ColCellGroup(0), SqrCellGroup(0)
+        c1 = Cell(rgrp, cgrp, sgrp)
+        c2 = Cell(rgrp, cgrp, sgrp)
+        c1._connectCells()
+        c2._connectCells()
+        self.assert_(c1.connected == set([c2]))
+        self.assert_(c2.connected == set([c1]))
+        return c1, c2
     
-    def testGetCol(self):
-        fn = '11-1.in'
-        pzl = SudokuPuzzle.fromRawFile(p.join(self.testd, fn))
-        expected = [0, 0, 0, 2, 7, 0, 1, 0, 9]
-        found = pzl._getCol(8)
-        self.assertTrue(found == expected)
-
-    def testGetGroup(self):
-        fn = '11-1.in'
-        pzl = SudokuPuzzle.fromRawFile(p.join(self.testd, fn))
-        expected = [0, 5, 0, 0, 0, 0, 0, 2, 0]
-        found = pzl._getGroup(5, 5)
-        self.assertTrue(found == expected)
+    def testCellUpdate(self):
+        c1, c2 = self.setup2cells()
+        c1.excludeVals(range(1,9))
+        self.assert_(c1.val == 9)
+        self.assert_(9 not in c2.possibles)
     
-    def testBruteSolve(self):
-        bruteSolve
+    def testCircularUpdate(self):
+        c1, c2 = self.setup2cells()
+        c1.excludeVals(range(1,8))
+        c2.excludeVals(range(1,8))
+        self.assert_(c1.possibles == set([8, 9]))
+        self.assert_(c2.possibles == c1.possibles)
+        c1.excludeVal(8)
+        self.assert_(c1.val == 9)
+        self.assert_(c2.val == 8)
+    
+#    def testCellUpdate(self):
+#        fn = '11-1.in'
+#        pzl = SudokuPuzzle()
+#        pzl.initFromBinFile(p.join(self.testd, fn))
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
